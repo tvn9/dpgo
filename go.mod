e8b9b1cf3455e55bee627ed5556adcccf45af5f5
Thanh Nguyen <tv.nguyen9@outlook.com>
2023-12-30 22:16:22 -0800

    Adding the singleton patter example code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dpgo
+
+go 1.21
---
+++ singleton/singleton.go
@@ -0,0 +1,60 @@
+// Demonstration of Singleton pattern
+package main
+
+import "fmt"
+
+// Vehicle hold information about Vehicle being built
+type Vehicle struct {
+	BodyType    string
+	Manufacture string
+	YearBuilt   string
+	VIN         string
+	PowerTrain  string
+	Color       string
+}
+
+// AddBodyType add, remove, or change the Vehicle Body Type
+func (v *Vehicle) AddBodyType(bt string) {
+	v.BodyType = bt
+}
+
+// AddManufacture adds the vehicle's manufacture name
+func (v *Vehicle) AddManufacture(mf string) {
+	v.Manufacture = mf
+}
+
+// AddYearBuilt adds the vehicle year built
+func (v *Vehicle) AddYearBuilt(yb string) {
+	v.YearBuilt = yb
+}
+
+// AddVIN adds the VIN info to the Vehicle object
+func (v *Vehicle) AddVIN(vn string) {
+	v.VIN = vn
+}
+
+// AddPowerTrain adds the vehicle's power train info
+func (v *Vehicle) AddPowerTrain(pt string) {
+	v.PowerTrain = pt
+}
+
+// AddColor add the vehicle's color
+func (v *Vehicle) AddColor(cl string) {
+	v.Color = cl
+}
+
+func main() {
+	// Create new vehicle
+	f150 := Vehicle{}
+
+	f150.AddBodyType("Truck")
+	f150.AddManufacture("Ford")
+	f150.AddYearBuilt("2021")
+	f150.AddVIN("2FADPJ9BM15693")
+	fmt.Println(f150)
+
+	// Remove year built
+	f150.AddYearBuilt("")
+	fmt.Println(f150)
+
+}
