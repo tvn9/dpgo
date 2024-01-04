f290fbf2fe7057c52342a136e760c3749b89b74c
Thanh Nguyen <tv.nguyen9@outlook.com>
2024-01-03 23:32:10 -0800

    Adding builder example

--- solid/isp/isp.go
+++ solid/isp/isp.go
@@ -4,7 +4,42 @@ package main
 type Document struct {
 }
 
-type Machine interface {
+// ISP
+type Printer interface {
+	Print(d Document)
+}
+
+type Scanner interface {
+	Scan(d Document)
+}
+
+type Faxer interface {
+	Fax(d Document)
+}
+
+type MyPrinter struct {
+}
+
+func (m MyPrinter) Print(d Document) {
+}
+
+type Photocopier struct{}
+
+func (p Phtocopier) Scan(d Document) {
+}
+
+func (p Photocopier) Print(d Document) {
+}
+
+type MyFax struct {
+}
+
+func (m MyFax) Fax(d Document) {}
+
+type AllInOnePrinter interface {
+	Printer
+	Scanner
+	Faxer
 }
 
 func main() {
